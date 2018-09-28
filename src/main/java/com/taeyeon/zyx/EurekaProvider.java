b05f639d8d3c5133f3410191e20f6e6614c6cbe0
zyxelva <zyxelva@gmail.com>
2018-09-28 16:54:08 +0800

    可用Demo完成

--- src/main/java/com/taeyeon/zyx/EurekaProvider.java
+++ src/main/java/com/taeyeon/zyx/EurekaProvider.java
@@ -3,44 +3,15 @@ package com.taeyeon.zyx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
-import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
 @ImportResource("classpath:/spring/applicationContext.xml")
-//@MapperScan("com.taeyeon.zyx.dao")
-//public class EurekaProvider extends SpringBootServletInitializer {
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(EurekaProvider.class);
-//	}
-
 public class EurekaProvider {
 	public static void main(String[] args) {
-		System.out.print("+++++++++++++++++++++++"+new Date()+"++++++++++++++++++++++++");
-//		new SpringApplicationBuilder(EurekaProvider.class).web(true).run(args);
+		System.out.print("++++++++++++++++++++++EurekaProvider Start+++++++++++++++++++++++++");
 		SpringApplication.run(EurekaProvider.class, args);
-		System.out.print("======================"+new Date()+"=========================");
+		System.out.print("======================EurekaProvider Start is Done=========================");
 	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-		PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
-		c.setIgnoreUnresolvablePlaceholders(true);
-		return c;
-	}
-////
-////	@Bean
-////	public FilterRegistrationBean resourceBaseUrlFilterRegistration() {
-////		FilterRegistrationBean registration = new FilterRegistrationBean();
-////		registration.setFilter(new ResourceBaseUrlFilter());
-////		registration.addUrlPatterns("/*");
-////		registration.setName("resourceBaseUrlFilter");
-////		registration.setOrder(2);
-////		return registration;
-////	}
 }
---
+++ src/main/java/com/taeyeon/zyx/controller/ClientReportControl.java
@@ -0,0 +1,36 @@
+package com.taeyeon.zyx.controller;
+
+import com.taeyeon.zyx.application.report.TbClientReportApplication;
+import com.taeyeon.zyx.dto.TbClientReportDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/student")
+public class ClientReportControl {
+
+    @Autowired
+    private TbClientReportApplication tbClientReportApplication;
+
+    @GetMapping("/testHello/{name}")
+    public String testHello(@PathVariable("name") String name) {
+        return "Hello world," + name;
+    }
+
+    @GetMapping("/getReportById/{id}")
+    public TbClientReportDto getReportById(@PathVariable("id") Long id) {
+        System.out.println("================this is one=====================get client report start. id:{}: " + id);
+        return tbClientReportApplication.get(id);
+    }
+
+    @GetMapping("/getReportById2/{id}")
+    public ResponseEntity<TbClientReportDto> getReportById2(@PathVariable("id") String id) {
+        System.out.println("================this is two=====================get client report start. id:{}: " + id);
+        return ResponseEntity.ok(tbClientReportApplication.get(Long.valueOf(id)));
+    }
+
+}
